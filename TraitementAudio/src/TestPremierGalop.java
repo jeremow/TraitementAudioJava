6570c3f31158016168ce3a055cd4f79ea5987f99
President <President@minatecap42-169.minatec.grenoble-inp.fr>
2017-04-12 14:55:18 +0200

    huhhhuhu

--- TraitementAudio/src/TestPremierGalop.java
+++ TraitementAudio/src/TestPremierGalop.java
@@ -1,23 +1,27 @@
 public class TestPremierGalop {
 	public static void main(String[] args) throws Exception {
-		GenSineBasic gen = new GenSineBasic(440/2, 1.0);
+		
+		Patch chapt = new Patch("Accord parfait majeur");
+		
+		GenSineBasic gen = new GenSineBasic(440, 1.0);
 		AudioDataReceiver output = new AudioDataReceiver();
-		GenSineBasic gen2 = new GenSineBasic(554/2, 0.8);
+		GenSineBasic gen2 = new GenSineBasic(554, 0.8);
 		AudioDataReceiver output2 = new AudioDataReceiver();
-		GenSineBasic gen3 = new GenSineBasic(659/2, 0.6);
+		GenSineBasic gen3 = new GenSineBasic(659, 0.6);
 		AudioDataReceiver output3 = new AudioDataReceiver();
 		
 		ModuleAbstract.connect(gen, 0, output, 0);
 		ModuleAbstract.connect(gen2, 0, output2, 0);
 		ModuleAbstract.connect(gen3, 0, output3, 0);
-		for (int i = 0; i < 44100*3 ; i++) {
-			gen.exec();
-			output.exec();
-			gen2.exec();
-			output2.exec();
-			gen3.exec();
-			output3.exec();
-		}
+		
+		chapt.addModule(gen);
+		chapt.addModule(gen2);
+		chapt.addModule(gen3);
+		chapt.addModule(output);
+		chapt.addModule(output2);
+		chapt.addModule(output3);
+		
+		chapt.exec(44100*5);
 
 		output.playAudioData();
 		output2.playAudioData();
